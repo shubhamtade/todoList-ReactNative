415d2faa7bf2920afc33433de81ffe8d032ddb0b
Shubham Sunil Tade <shubham.tade@gmail.com>
2021-08-20 19:58:33 +0530

    Update Code

--- components/Task.js
+++ components/Task.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
-const Task = (msg) => {
+const Task = (props) => {
 
-    return(
-        <View style={styles.item}>
-        <View style={styles.itemLeft}>
-            <View style={styles.square}></View>
-            <Text style={styles.itemText}>{msg.text}</Text>
-        </View>
-        <View style={styles.circular}></View>
-        </View>
-    )
+  return (
+    <View style={styles.item}>
+      <View style={styles.itemLeft}>
+        <View style={styles.square}></View>
+        <Text style={styles.itemText}>{props.text}</Text>
+      </View>
+      <View style={styles.circular}></View>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-item:{
+  item: {
     backgroundColor: '#FFF',
     padding: 15,
     borderRadius: 10,
@@ -23,31 +23,30 @@ item:{
     alignItems: 'center',
     justifyContent: 'space-between',
     marginBottom: 20,
-},
-itemLeft:{
+  },
+  itemLeft: {
     flexDirection: 'row',
-    alignItem: 'center',
-    flexWrap: 'wrap',
-},
-square:{
+    alignItems: 'center',
+    flexWrap: 'wrap'
+  },
+  square: {
     width: 24,
     height: 24,
-    backgroundColor: 'red',
+    backgroundColor: 'rgb(232,136,17)',
     opacity: 0.4,
     borderRadius: 5,
     marginRight: 15,
-},
-text:{
+  },
+  itemText: {
     maxWidth: '80%',
-
-},
-circular:{
+  },
+  circular: {
     width: 12,
     height: 12,
-    borderColor: 'red',
+    borderColor: 'rgb(232,136,17)',
     borderWidth: 2,
     borderRadius: 5,
-},
+  },
 });
 
 export default Task;
